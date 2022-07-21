478c4cfd85022fa87a601fecc23361d036a150ff
Gyuddi <90381760+Gyuddi@users.noreply.github.com>
2022-07-21 16:15:40 +0900

    [SolvedX]규현_부분 문자열

---
+++ 2022년07월20일/BOJ_6550_H.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BOJ_6550_H {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            String s = sc.next();
+            String t = sc.next();
+            int idx = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(idx) == t.charAt(i)){
+                    idx++;
+                }
+                if (idx == s.length()){
+                    break;
+                }
+            }
+            if (idx == s.length()){
+                System.out.println("Yes");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+
+    }
+}
