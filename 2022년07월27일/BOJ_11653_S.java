7f76aab9a8cb7e49381d56aa6e15bc208ad24dab
Sujin Shin <97040053+JinJinee@users.noreply.github.com>
2022-07-28 20:22:53 +0900

    [Solved✌🏻]수진_소인수분해
    - 메모리 : 14400KB
    - 속도 : 176ms

---
+++ 2022년07월27일/BOJ_11653_S.java
@@ -0,0 +1,28 @@
+// 소인수분해
+package Beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11653_S {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int tmp = 2;
+
+        while(N > 1){
+            if(N % tmp == 0){
+                sb.append(tmp).append("\n");
+                N /= tmp;
+            }else{
+                tmp++;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
