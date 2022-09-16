9f08270b925c3ad1aa405bdccc6ea6974b12ffb4
Gyuddi <90381760+Gyuddi@users.noreply.github.com>
2022-09-16 18:49:32 +0900

    15일 [Solved✌🏻]수진_튜플

---
+++ 2022년09월16일/KAKAO_Tuple_H.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class KAKAO_Tuple_H {
+    public static void main(String[] args) {
+        String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+        solution(s)
+    }
+    public static int[] solution(String s) {
+        s= s.replace("{","");
+        s=s.replace("},","-");
+        s=s.replace("}}","");
+        String[] arr = s.split("-");
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length()-o2.length();
+            }
+        });
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            String[] a = arr[i].split(",");
+            for (int j = 0; j < a.length; j++) {
+                if (!list.contains(a[j])){
+                    list.add(a[j]);
+                }
+            }
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = Integer.parseInt(list.get(i));
+        }
+        return answer;
+    }
+}
