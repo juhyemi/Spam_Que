3d025f1f94cdfd635a7dd1561484e4a22ac87699
juhyemi <114149911+juhyemi@users.noreply.github.com>
2022-09-22 21:47:52 +0900

    [Solved✌🏻]주혜_We love kriii

---
+++ 2022년09월22일/BOJ_10718_J.java
@@ -0,0 +1,10 @@
+
+public class BOJ_10718_J {
+	public static void main(String[] args) {
+		
+		System.out.printf("%s %s\n","강한친구","대한육군");
+		System.out.printf("%s %s\n","강한친구","대한육군");
+	}
+	
+
+}
