b18ccd5d6fa22b66b61eebce9e8073eec3f8ea34
juhyemi <114149911+juhyemi@users.noreply.github.com>
2022-09-22 21:21:03 +0900

    [Solved✌🏻]주혜_A-B

---
+++ 2022년09월22일/BOJ_1001_J.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_1001_J {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt(); 
+		int B = sc.nextInt();
+		
+		System.out.println(A-B);
+		
+	}
+
+}
