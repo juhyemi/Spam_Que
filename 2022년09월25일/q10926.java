81924a4d4883049cd08d07a9f34f00ee674e0beb
juhyemi <114149911+juhyemi@users.noreply.github.com>
2022-09-25 23:25:48 +0900

    [Solved✌🏻]주혜_??!

---
+++ 2022년09월25일/q10926.java
@@ -0,0 +1,13 @@
+package stage1;
+
+import java.util.Scanner;
+
+public class q10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.printf("%s" , id + "??!");
+		
+	}
+
+}
