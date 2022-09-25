e57ee2172260587af4d9f778424c37c84293a027
juhyemi <114149911+juhyemi@users.noreply.github.com>
2022-09-25 23:24:47 +0900

    [Solved✌🏻]주혜_A/B

---
+++ 2022년09월25일/q1008.java
@@ -0,0 +1,15 @@
+package stage1;
+
+import java.util.Scanner;
+
+public class q1008 {
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	double A = sc.nextDouble();
+	double B = sc.nextDouble();
+	System.out.println(A/B);
+
+	}
+
+}
