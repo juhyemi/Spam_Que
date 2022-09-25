a36fcb6f6866f06e1f4e10723ea81c1783428288
juhyemi <114149911+juhyemi@users.noreply.github.com>
2022-09-25 23:25:21 +0900

    [Solved✌🏻]주혜_사칙연산

---
+++ 2022년09월25일/q10869.java
@@ -0,0 +1,17 @@
+package stage1;
+
+import java.util.Scanner;
+
+public class q10869 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B); //이거를 한줄에 \n 써서 출력하고 싶은데 printf로 써도 계속 에러나유ㅠ도와줘잉
+	}
+
+}
