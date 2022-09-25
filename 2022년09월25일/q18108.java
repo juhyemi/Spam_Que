c5afeb20121f52f3b6ee2ab3e53a923628367534
juhyemi <114149911+juhyemi@users.noreply.github.com>
2022-09-25 23:26:33 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ 2022년09월25일/q18108.java
@@ -0,0 +1,12 @@
+package stage1;
+
+import java.util.Scanner;
+
+public class q18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		System.out.println(y - 543);
+	}
+
+}
