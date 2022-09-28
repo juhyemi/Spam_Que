7440ebfa18f828d6d95bbbff5655df43b5a33dc0
Gyuddi <90381760+Gyuddi@users.noreply.github.com>
2022-09-28 20:10:57 +0900

    [Solved✌🏻]규현_돌 게임

---
+++ 2022년09월28일/BOJ_9655_H.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BOJ_9655_H {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N%2 == 0) System.out.println("CY");
+        else System.out.println("SK");
+    }
+}
