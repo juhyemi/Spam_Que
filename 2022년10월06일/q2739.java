b8892584bba6c92fd5f355be843ff0af820c0025
juhyemi <114149911+juhyemi@users.noreply.github.com>
2022-10-06 21:54:00 +0900

    [Solved✌🏻]주혜_구구단

---
+++ 2022년10월06일/q2739.java
@@ -0,0 +1,19 @@
+package Bjalgorism;
+
+import java.util.Scanner;
+
+public class q2739 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int d = sc.nextInt();
+		
+		for (int i =1;i<10;i++) {
+			System.out.println(d +" * "+ i + " = " + d*i);
+		}
+		
+		
+		
+	}
+
+}
